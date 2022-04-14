600aa1a7cf44036c02b0ca068c664fd4d485825b
satells <msergiost@hotmail.com>
2022-04-14 10:36:18 -0300

    Start.

---
+++ sectes/src/main/java/com/sectes/SectesApplication.java
@@ -0,0 +1,13 @@
+package com.sectes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectesApplication.class, args);
+	}
+
+}
